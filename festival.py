338b0a6ba063bf98ab2b0466960fa0d2699a75b5
Ricardo De Pena <ricardo.depena@gmail.com>
2013-07-16 23:56:23 -0400

    removed comments/added comments

--- festival.py
+++ festival.py
@@ -3,6 +3,6 @@ import os
 
 class festival():
 
-	
+	#you will need festival installed on the machine.
 	def say(self,text):
 		os.system("echo " + str(text) + " | festival --tts")
--- sayWeather.py
+++ sayWeather.py
@@ -8,8 +8,6 @@ def main():
     forecast = Forecastio("70f2d5ee3ee858d5869d2038f6001dad")
     result = forecast.load_forecast(40.697488,-73.979681,
                                    time=datetime.datetime.now(), units="us")
-    #print result
-
 
     if result['success'] is True:
 
@@ -21,9 +19,6 @@ def main():
         voice.say("Hourly Summary.")
         voice.say(by_hour.summary)
 
-        # for hourly_data_point in by_hour.data:
-        #     print hourly_data_point
-
         # print "===========Daily Data========="
         by_day = forecast.get_daily()
         print "Daily Summary: %s" % (by_day.summary)
@@ -32,7 +27,6 @@ def main():
 
         for daily_data_point in by_day.data:
             print daily_data_point
-            #engine.say(daily_data_point)
     else:
         print "A problem occurred communicating with the Forecast.io API"
 
